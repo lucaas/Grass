5d595c7e5ab0a9cc6f2a05e0925ee1180b0fe08e
David Jonsson <davidjons@gmail.com>
2010-01-25 14:39:50 +0100

    La till mellanrum

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ static void display(void)
         glBegin(GL_LINES);
             glColor3f(1.0f,0.0f,0.0f);
             glVertex3f(0.0f, 0.0f, -10.0f);
-            glColor3f(0.0f,1.0f,0.0f);
+            glColor3f(0.0f, 1.0f, 0.0f);
             glVertex3f(0.5f, 0.5f, -10.0f);
         glEnd();
     glPopMatrix();
